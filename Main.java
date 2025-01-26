d19b592b66c4e605162a54d1872185f24d52134d
Vedant R <110495968+Blue-Man-08@users.noreply.github.com>
2025-01-26 22:56:42 +0000

    got rid of comments and fluff. Completed versiona

--- Main.java
+++ Main.java
@@ -4,16 +4,7 @@ import java.util.Arrays;
 public class Main {    
     public static void main(String[] args)
     {
-        // Task:
-        // 1. Implement missing sorting algorithms.
-        //    - InsertionSort.java
-        //    - SelectionSort.java
-        //
-        //  2. Add a method to class Main to generate a random array.
-        //  3. Provide random input to test suite and ensure your implementations
-        //     of InsertionSort and Selection sort continue to work.
-        //  4. Increase the loop count for TestSuite to get better performance results.
-        // Increase the loop count to get better results once it works.
+
         TestSuite.run(randomArrayGen(1000, 1000), 10000);
     }
     public static int[] randomArrayGen(int numRandoms, int highNum){
